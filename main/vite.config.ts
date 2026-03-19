19d5eae3b26cbab200681f05f5d954f10ab03c8d
teacherator <aronsond@ramaz.org>
2026-03-19 15:57:37 -0400

    deployemnt fix

--- main/vite.config.ts
+++ main/vite.config.ts
@@ -4,6 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    port: process.env.PORT ? parseInt(process.env.PORT) : 8080,
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: process.env.PORT ? parseInt(process.env.PORT) : 8080,
+  },
   test: {
     globals: true,
     environment: 'jsdom',
