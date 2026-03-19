e36cd568ec778db8e4e76ca822d6106fb2e96cf8
teacherator <aronsond@ramaz.org>
2026-03-19 16:03:01 -0400

    domain stuff

--- main/vite.config.ts
+++ main/vite.config.ts
@@ -11,6 +11,7 @@ export default defineConfig({
   preview: {
     host: '0.0.0.0',
     port: process.env.PORT ? parseInt(process.env.PORT) : 8080,
+    allowedHosts: ['lionfish-app-5f4rk.ondigitalocean.app', 'kohack2026.ramaztec.org'],
   },
   test: {
     globals: true,
