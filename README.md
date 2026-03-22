e5422b78af19333c7676b20c8b93347ce66049a2
InventorProgram <heilborn-nigrih@ramaz.org>
2026-03-22 16:45:21 -0400

    fixed weird git issue?

--- README.md
+++
@@ -1,6 +0,0 @@
-# kohack2026
-front end repo for kohack
-
-docs for backend api: https://api.sizebud.com/docs 
-
-site link: https://lionfish-app-5f4rk.ondigitalocean.app/ 
--- main/src/setupTests.ts
+++
@@ -1,5 +0,0 @@
-import '@testing-library/jest-dom'
-import { TextEncoder, TextDecoder } from 'util'
-
-;(globalThis as any).TextEncoder = TextEncoder
-;(globalThis as any).TextDecoder = TextDecoder
